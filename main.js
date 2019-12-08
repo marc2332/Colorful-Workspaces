8d1b590be80829e22873e4bc3b2138271ce4c136
Marc Espín <mespinsanz@gmail.com>
2019-12-08 16:14:24 +0100

    Made it compatible with last Graviton v1.2.0

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-if(GravitonInfo < "191004") {
+if(GravitonInfo < "191208") {
   graviton.consoleWarn("Colorful Workspaces does not work on your Graviton.")
   return
 };
@@ -45,51 +45,64 @@ plugin.getData((data)=>{
   data.list.forEach((one)=>{
      content += `
      <div class="section-1">
-         <p style=" margin:5px 0px;">${one.path.replace(/\\/g, "\\\\")}</p>
+         <p style=" margin:5px 0px;">${one.path.replace(/\\/g, "\\")}</p>
          <div style="display:inline-block; background:rgb(${one.color.r},${one.color.g},${one.color.b}); width:15px; height:15px;border-radius:50px;"></div>
-         <input style="margin:5px 0px;  display:inline-block;" class=input4 value="${one.color.r},${one.color.g},${one.color.b}"></input>
+         <input style="margin:5px 0px;  display:inline-block;" class="input4" value="${one.color.r},${one.color.g},${one.color.b}"></input>
       </div>
       <span class="line_space_menus"></span>
      `
   })
-  const workspaces_sec = Settings.addNewSection({
-    name:"ColorfulWorkspaces",
-    content:`
-    <h3>Saved workspaces:</h3>
-    ${content}
-    `
-  })
-  workspaces_sec.on('load', () => {
-    const page = document.getElementById("settings.ColorfulWorkspaces");
-    for(i=0;i<page.children.length;i++){
-      if( page.children[i].classList == "section-1"){
-        const section = page.children[i]
-        const input = page.children[i].children[2];
-        input.oninput = (button) =>{
-          section.children[1].style.background = input.value
-          plugin.getData(function(data) {
-        		const list = data.list
-        		const project = list.filter((item)=>{
-              return item.path == section.children[0].textContent
-            })[0]
-            project.color = tinycolor(input.value).toRgb();
-            loadFromList(list,project.path)
-        	})
-        }
-        input.onchange = ()=> {
-          plugin.getData(function(data) {
-        		const list = data.list
-        		const project = list.filter((item)=>{
-              return item.path == section.children[0].textContent
-            })[0]
-            project.color = tinycolor(input.value).toRgb();
-        		plugin.saveData({
-        			list: list
-        		})
-            loadFromList(list,project.path)
-        	})
-        }
-      }
-    }
-  })
+	const workspaces_dm = new dropMenu({
+		id:"my_dropmenu"
+	});
+	workspaces_dm.setList({
+		"button": "Workspaces",
+		"list":{
+			 "Click me!":{
+					click:function(){
+						const ws_window = new Window({
+							content:`
+								<div class="section-4" id="colorfulworkspaces">
+									<h3>Saved workspaces:</h3>
+									${content}
+								</div>
+							`
+						})
+						ws_window.launch()
+						const page = document.getElementById("colorfulworkspaces");
+						for(i=0;i<page.children.length;i++){
+							if( page.children[i].classList == "section-1"){
+								const section = page.children[i]
+								const input = page.children[i].children[2];
+								input.oninput = (button) =>{
+									section.children[1].style.background = input.value
+									plugin.getData(function(data) {
+										const list = data.list
+										const project = list.filter((item)=>{
+											return item.path == section.children[0].textContent
+										})[0]
+										project.color = tinycolor(input.value).toRgb();
+										loadFromList(list,project.path)
+									})
+								}
+								input.onchange = ()=> {
+									plugin.getData(function(data) {
+										const list = data.list
+										const project = list.filter((item)=>{
+											return item.path == section.children[0].textContent
+										})[0]
+										project.color = tinycolor(input.value).toRgb();
+										plugin.saveData({
+											list: list
+										})
+										loadFromList(list,project.path)
+									})
+								}
+							}
+						}	
+					}
+				}
+		}
+	})
+
 })
